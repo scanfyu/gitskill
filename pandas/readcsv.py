e654b48f3578475fc71e55c2100e8ed6b6fc285f
scanfyu <songseu@outlook.com>
2017-02-08 15:50:12 +0800

    stock

---
+++ pandas/readcsv.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+def test_run():
+    """Function called by Test Run."""
+    df = pd.read_csv("data/AAPL.csv")
+    print df.tail()  # TODO: Print last 5 rows of the data frame
+
+
+if __name__ == "__main__":
+    test_run()
