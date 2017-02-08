35d143a4355cfe980ce7385ca35fd9b83507d3ba
scanfyu <songseu@outlook.com>
2017-02-08 18:01:02 +0800

    data

---
+++ pandas/Utility.py
@@ -0,0 +1,38 @@
+"""Utility functions"""
+
+import os
+import pandas as pd
+
+def symbol_to_path(symbol, base_dir="data"):
+    """Return CSV file path given ticker symbol."""
+    return os.path.join(base_dir, "{}.csv".format(str(symbol)))
+
+
+def get_data(symbols, dates):
+    """Read stock data (adjusted close) for given symbols from CSV files."""
+    df = pd.DataFrame(index=dates)
+    if 'SPY' not in symbols:  # add SPY for reference, if absent
+        symbols.insert(0, 'SPY')
+
+    for symbol in symbols:
+        def_temp = pd.read.csv(symbol_to_path(symbol), index_col='Data',
+                    parse_dates=True, useTools=['Data', 'Adj Close'],nan_value=['nan'])
+        # TODO: Read and join data for each symbol
+
+    return df
+
+
+def test_run():
+    # Define a date range
+    dates = pd.date_range('2010-01-22', '2010-01-26')
+
+    # Choose stock symbols to read
+    symbols = ['GOOG', 'IBM', 'GLD']
+    
+    # Get stock data
+    df = get_data(symbols, dates)
+    print df
+
+
+if __name__ == "__main__":
+    test_run()
--- pandas/mean.py
+++ pandas/mean.py
@@ -9,7 +9,7 @@ def get_mean_volume(symbol):
     """
     df = pd.read_csv("data/{}.csv".format(symbol))  # read in data
     # TODO: Compute and return the mean volume for this stock
-    return df['Mean']
+    return df['Volume'].mean() # compute and return mean
 
 
 def test_run():
