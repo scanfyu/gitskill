337324ecffd6f5e1c3ce9bb4b077b9da6fbbf9ec
scanfyu <songseu@outlook.com>
2017-02-09 22:14:44 +0800

    data

--- data/csv.py
+++ data/csv.py
@@ -15,9 +15,21 @@ DATAFILE = "beatles-diskography.csv"
 
 def parse_file(datafile):
     data = []
-    with open(datafile, "r") as f:
+    with open(datafile, "rb") as f:
+        header = f.readline().split(",")
+        counter = 0
         for line in f:
-            print line
+            if counter == 10:
+                break
+
+            fields = line.split(",")
+            entry = {}
+
+            for i, value in enumerate(fields):
+                entry[header[i].strip()] = value.strip()   #去除空格,包括末尾换行符
+
+            data.append(entry)
+            counter +=1
 
     return data
 
