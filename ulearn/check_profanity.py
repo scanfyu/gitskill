7cf28988dbb56e4d01fd2dad8f9af192d015e564
scanfyu <songseu@outlook.com>
2017-02-10 11:48:47 +0800

    code

--- ulearn/check_profanity.py
+++ ulearn/check_profanity.py
@@ -1,3 +1,5 @@
+import urllib
+
 def read_text():
     quotes = open("F:\OOP\movie_quotes.txt")
     contents_of_file = quotes.read()
@@ -8,7 +10,13 @@ def read_text():
 def check_profanity(text_to_check):
     connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
     output = connection.read()
-    print(output)
+    # print(output)
     connection.close()
+    if "true" in output:
+        print("用语警告！")
+    elif "false" in output:
+        print("本文用语无问题！")
+    else:
+        print("无法正常浏览文档！")
 
 read_text()
