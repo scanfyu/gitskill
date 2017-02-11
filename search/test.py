02d4f5c5107592aa05a0e3fa59c79da46e5680be
scanfyu <songseu@outlook.com>
2017-02-11 23:24:19 +0800

    code

--- search/test.py
+++ search/test.py
@@ -1,9 +1,16 @@
 
 
 # str.find(str, beg=0, end=len(string)) 方法介绍
-'''
+''' 
 text = "all zip files are zipped" 
 print text.find('zip',text.find('zip') + 1)
+'''
+
+'''
+寻找第二个
+def find_second(search, target):
+    first = search.find(target)
+    return search.find(target, first + 1)
 '''
 
 page = ('<div id="top_bin"><div id="top_content" class="width960">'
@@ -14,6 +21,11 @@ end_quote = page.find('"', start_quote + 1) # 链接结尾的引号
 url = page[start_quote+1 : end_quote] # 输出引号之间的内容
 
 print url
+
+# 寻找下一个出现位置
+danton = "De l'audance,"
+
+print find_second(danton, 'audance')
 
 # def <name>(<parameters>输入/input-> <name>, <name>, ...):
 #     <block>
@@ -35,3 +47,27 @@ num = x + 0.5
 s = str(num)
 point = s.find('.')
 print s[:point]
+
+# 寻找第一个字母是D的
+def is_friend(name):
+    return name[0] == 'D' or name[0] == 'N'
+
+print is_friend('Diane')
+
+# 输出最大值
+def biggest(a, b, c):
+    return bigger(bigger(a, b), c)
+
+max(a, b, c)
+
+def bigger(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+# while 介绍
+i = 0
+while i < 10:
+    print i
+    i = i + 1
--- upy/spyder.py
+++ upy/spyder.py
@@ -5,3 +5,6 @@ def get_next_target(self):
     start_quote = self.find('"', start_link)             # 链接前引号位置
     end_quote = self.find('"', start_quote + 1)          # 链接结尾的引号
     url = self[start_quote+1 : end_quote]                # 输出引号之间的内容
+    return url, end_quote
+
+
